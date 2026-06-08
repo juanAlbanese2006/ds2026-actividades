7afc03fb0fcef570a88bb9ae66510c1bad86f975
juanAlbanese2006 <juanAlbanese9@gmail.com>
2026-06-07 23:06:16 -0300

    feat: routing con páginas

---
+++ actividades/co9/c08-libreria-react/src/Pages/Catalogo.tsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+
+const Catalogo = () => (
+  <Container className="my-5">
+    <h2>📖 Catálogo de Libros</h2>
+    <p>Aquí iría la lista completa de libros.</p>
+  </Container>
+);
+
+export default Catalogo;
--- actividades/co9/c08-libreria-react/src/main.tsx
+++ actividades/co9/c08-libreria-react/src/main.tsx
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+
+
 
